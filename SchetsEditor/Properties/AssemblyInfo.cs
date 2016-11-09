ee051b7daa9c8105546d8dcba72d7faeb20ddebd
Gericom <f.nouwt@dshack.org>
2016-11-09 22:23:32 +0100

    Cleaned up

--- SchetsEditor/Properties/AssemblyInfo.cs
+++ SchetsEditor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Schets")]
+[assembly: AssemblyTitle("Schets++")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Schets")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Schets++")]
+[assembly: AssemblyCopyright("Copyright Florian and Ruben ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
